17c930a0068d6b3b4e086c51e717010693f4b3bc
Abdul Aziz Tetteh <abdulaziztetteh@gmail.com>
2020-12-13 15:39:46 +0000

    Add conditionals and cloudwatch

--- services/webserver-cluster/variables.tf
+++ services/webserver-cluster/variables.tf
@@ -8,20 +8,22 @@ variable "cluster_name" {
   description = "The name to use for all the cluster resources"
   type        = string
 }
+
 variable "db_remote_state_bucket" {
   description = "The name of the S3 bucket for the database's remote state"
   type        = string
 }
+
 variable "db_remote_state_key" {
   description = "The path for the database's remote state in S3"
   type        = string
 }
 
-
 variable "instance_type" {
   description = "The type of EC2 Instances to run (e.g. t2.micro)"
   type        = string
 }
+
 variable "min_size" {
   description = "The minimum number of EC2 Instances in the ASG"
   type        = number
@@ -36,5 +38,9 @@ variable "custom_tags" {
   description = "Custom tags to set on the instances in the ASG"
   type        = map(string)
   default     = {}
+}
 
+variable "enable_autoscaling" {
+  description = "If set to true, enable auto scaling"
+  type        = bool
 }
